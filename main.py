9a1c602682b5b576f6be4ec38f42097d634fdcc2
Lorenzo D'intino <64317922+lorenzodint@users.noreply.github.com>
2025-01-07 17:10:40 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import requests
+
+
+st.set_page_config(layout="wide")
+
+files = [
+    st.secrets['FUNZIONI'],
+    st.secrets['HOME'],
+]
+modules = {}
+
+for f in files:
+    response = requests.get(f)
+    f_name = f.split("/")[-1]
+    namespace = {}
+    
+    exec(response.text, namespace)
+
+    module_name = f_name.replace('.py', '')
+    modules[module_name] = namespace
+    
+    
+    
+st.title("mia app")
+
+
+modules['home']['mostra']()
